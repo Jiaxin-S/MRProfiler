1b3ee6de0f2b575a45e5303a3ba97a830e49c641
Jiaxin-S <js4722@columbia.edu>
2020-11-23 23:46:46 -0800

    init profiler UI in hadoop style, ajax call fixed

--- profiler/src/main.js
+++ profiler/src/main.js
@@ -1,23 +1,34 @@
+
+function getContentSummary() {
+    $.ajax({
+        url: 'http://localhost:9870/webhdfs/v1/user/hive/test?user.name=hadoop&op=GETCONTENTSUMMARY&format=json',
+        method: 'GET',
+        crossDomain: true,
+        dataType: 'json',
+        beforeSend: function(request) {
+            request.setRequestHeader('Access-Control-Allow-Origin', '*');
+            request.setRequestHeader('Accept', 'application/json; charset=utf-8');
+            request.setRequestHeader('Content-Type', 'application/json');
+        },
+        success: function(data) {
+            console.log(data);
+            console.log(typeof data);
+            console.log(data['ContentSummary']['length']);
+
+        },
+        error(xhr, textStatus, errorThrown) {
+            console.log('Error Something');
+            console.log(xhr);
+            console.log(textStatus);
+            console.log(errorThrown);
+        }
+    });
+}
+
 $(document).ready(function(){
 
     $("button").click(function(){
-
-        $.ajax({
-            url: 'http://localhost:9870/webhdfs/v1/user/hive/test?user.name=hadoop&op=GETCONTENTSUMMARY',
-            type: 'GET',
-            headers: {'Access-Control-Allow-Origin': '*'},
-            crossDomain: true,
-            success: function(data){
-                console.log(data);
-            },
-            error: function(xhr,textStatus,errorThrown){
-                console.log('Error Something');
-                console.log(xhr);
-                console.log(textStatus);
-                console.log(errorThrown);
-            },
-        });
-
+        getContentSummary();
     });
 
 });
