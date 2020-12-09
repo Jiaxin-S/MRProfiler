e442ff1a0991189fa7eef7b407f409e514b2b0ad
eddiehuang97 <eddiehuang97@github.com>
2020-12-09 13:22:26 -0800

    fixed cli bug

--- cli/cli/cli.py
+++ cli/cli/cli.py
@@ -15,7 +15,7 @@ def main():
     if args.op == 'GETRESOURCEMANAGER':
         data = requests.get('http://localhost:8088/ws/v1/cluster/info').json()
     elif args.op == 'GETNODEMANAGER':
-        data = requests.get('http://localhost:8088/ws/v1/cluster/info').json()
+        data = requests.get('http://localhost:8042/ws/v1/node/info').json()
     else:
         data = requests.get('http://%s:%s/webhdfs/v1/%s?user.name=%s&op=%s' % (HOST, PORT, args.path, args.user, args.op)).json()
     print(json.dumps(data, indent=2))
