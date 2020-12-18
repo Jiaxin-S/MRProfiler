2ee2927e28cff1d5081a790b711388076a251547
eddiehuang97 <eddiehuang97@github.com>
2020-12-18 02:09:06 -0800

    email service

--- cli/cli/cli.py
+++ cli/cli/cli.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 import requests
 import json
+import smtplib, ssl
 
 HOST='localhost'
 PORT='9870'
@@ -16,6 +17,24 @@ def main():
         data = requests.get('http://localhost:8088/ws/v1/cluster/info').json()
     elif args.op == 'GETNODEMANAGER':
         data = requests.get('http://localhost:8042/ws/v1/node/info').json()
+    elif args.op == 'EMAILSERVICE':
+        while True:
+            data = requests.get('http://%s:%s/webhdfs/v1/%s?user.name=%s&op=%s' % (HOST, PORT, args.path, args.user, 'GETQUOTAUSAGE')).json()
+            if data['QuotaUsage']['spaceConsumed'] > 1000000:
+            break
+            sleep(10)
+        port = 465
+        smtp_server = "smtp.gmail.com"
+        sender_email = "cs239mrprofiler@gmail.com"
+        receiver_email = "huang.eddie@yahoo.com"
+        password = "239h@doop"
+        message = """\
+Subject: Hadoop Usage Alert
+
+Usage exceeded 1 GB"""
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message)
     else:
         data = requests.get('http://%s:%s/webhdfs/v1/%s?user.name=%s&op=%s' % (HOST, PORT, args.path, args.user, args.op)).json()
     print(json.dumps(data, indent=2))
